dfab3af0d20918d7e7faae56e5abe3d7bce72643
Alin Florin <alin@qoffice.ro>
2022-01-10 17:14:52 +0200

    -

--- MarsOffice.Microfunction/OpaExtensions.cs
+++ MarsOffice.Microfunction/OpaExtensions.cs
@@ -1,16 +1,62 @@
 using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 using MarsOffice.Microfunction;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class OpaExtensions
     {
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(r =>
+                {
+                    using var contentStream = r.Content.ReadAsStream();
+                    if (contentStream == null || contentStream.Length == 0)
+                    {
+                        return false;
+                    }
+                    using var ms = new MemoryStream();
+                    contentStream.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    var json = Encoding.UTF8.GetString(bytes);
+                    if (string.IsNullOrEmpty(json))
+                    {
+                        return false;
+                    }
+                    if (json == "{}" || !json.ToLower().Contains("decision"))
+                    {
+                        return true;
+                    }
+                    if (contentStream.CanSeek)
+                    {
+                        try
+                        {
+                            contentStream.Seek(0, SeekOrigin.Begin);
+                        }
+                        catch (Exception)
+                        {
+                            // ignored
+                        }
+                    }
+                    return false;
+                })
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
         public static IServiceCollection AddOpaClient(this IServiceCollection services, string opaBaseUrl, string opaToken)
         {
             services.AddHttpClient("OPA", (svc, hc) =>
                 {
                     hc.BaseAddress = new Uri(opaBaseUrl);
-                }).AddHttpMessageHandler((svc) => new OpaHttpMessageHandler(opaToken));
+                })
+                .AddHttpMessageHandler((svc) => new OpaHttpMessageHandler(opaToken))
+                .AddPolicyHandler(GetRetryPolicy());
             return services;
         }
     }
